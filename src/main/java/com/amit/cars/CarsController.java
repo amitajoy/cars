2d0a6df7034b87dfc50fefecaee9120ec6df25aa
Amit Agrawal <aagrawal@lendingclub.com>
2018-04-13 14:08:40 -0700

    code reformatting

--- src/main/java/com/amit/cars/CarsController.java
+++ src/main/java/com/amit/cars/CarsController.java
@@ -1,6 +1,9 @@
 package com.amit.cars;
 
 import com.amit.cars.model.Car;
+import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.ApiResponse;
+import io.swagger.annotations.ApiResponses;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -23,24 +26,48 @@ public class CarsController {
     @Inject
     private CarService carService;
 
-    @RequestMapping(method = {RequestMethod.GET, RequestMethod.POST})
+    @ApiOperation(value = "Returns a list of cars", responseContainer = "List", response = Car.class)
+    @ApiResponses({
+            @ApiResponse(code = 200, message = "Success", responseContainer = "List", response = Car.class),
+            @ApiResponse(code = 400, message = "Bad Request"),
+            @ApiResponse(code = 404, message = "Car does not exist"),
+            @ApiResponse(code = 500, message = "Unexpected failure")})
+    @RequestMapping(method = {RequestMethod.GET})
     public ResponseEntity<List<Car>> getCars(){
         List<Car> cars = carService.getCars();
         return ResponseEntity.ok().body(cars);
     }
 
-    @RequestMapping(method = {RequestMethod.GET, RequestMethod.POST} , value = "/{carid}")
+    @ApiOperation(value = "Returns a car by ID", response = Car.class)
+    @ApiResponses({
+            @ApiResponse(code = 200, message = "Success",response = Car.class),
+            @ApiResponse(code = 400, message = "Bad Request"),
+            @ApiResponse(code = 404, message = "Car does not exist"),
+            @ApiResponse(code = 500, message = "Unexpected failure")})
+    @RequestMapping(method = {RequestMethod.GET} , value = "/{carid}")
     public ResponseEntity<Car> getCarById(@PathVariable("carid") Integer carId){
         Car car = carService.getCar(carId);
         return ResponseEntity.ok().body(car);
     }
 
+    @ApiOperation(value = "Adds a car", response = Car.class)
+    @ApiResponses({
+            @ApiResponse(code = 200, message = "Success", response = Car.class),
+            @ApiResponse(code = 400, message = "Bad Request"),
+            @ApiResponse(code = 404, message = "Car does not exist"),
+            @ApiResponse(code = 500, message = "Unexpected failure")})
     @RequestMapping(method = {RequestMethod.PUT} , value = "/{carid}/{carname}")
     public ResponseEntity<Car> addCar(@PathVariable("carid") Integer carId,@PathVariable("carname") String carName){
         Car car = carService.addCar(carId, carName);
         return ResponseEntity.ok().body(car);
     }
 
+    @ApiOperation(value = "Removes a car",  response = Void.class)
+    @ApiResponses({
+            @ApiResponse(code = 200, message = "Success", response = Void.class),
+            @ApiResponse(code = 400, message = "Bad Request"),
+            @ApiResponse(code = 404, message = "Car does not exist"),
+            @ApiResponse(code = 500, message = "Unexpected failure")})
     @RequestMapping(method = {RequestMethod.DELETE} , value = "/{carid}")
     public ResponseEntity removeCar(@PathVariable("carid") Integer carId){
         carService.removeCar(carId);
